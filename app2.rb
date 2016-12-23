cc4a3d3d1713780176c09a96b6e264bce4d01efd
Emiel Estievenart <emiel-perijah@hotmail.com>
2016-12-23 20:21:37 +0100

    added the mqtt class back, gem now works on different pc's after installing mqtt

--- app2.rb
+++ app2.rb
@@ -2,7 +2,6 @@ require 'temp_converter.rb'
 require 'net/http'
 require 'uri'
 require 'rubygems'
-require 'mqtt'
 require 'json'
 require 'optparse'
 require 'text_reader.rb'
@@ -10,6 +9,7 @@ require 'url_reader.rb'
 require 'text_printer.rb'
 require 'json_printer.rb'
 require 'html_printer.rb'
+require 'mqtt_reader.rb'
 
 globalTemperature = 0
 path = 'C:\Users\Gebruiker\Documents\School\systeem\Oefenigen\01 Temp converter\temperatuur.txt'
@@ -48,15 +48,17 @@ end
 
   opts.on("-m","--mqtt", "temperature van mqtt") do
 
-    client.host = 'staging.thethingsnetwork.org'
-    client.port = '1883'
-    client.username = '70B3D57ED00012B2'
-    client.password = 'c8iuTSccnypK1eoFzEb/OoqB2FVAiFg/aEaYesnNf4w='
-    client.connect
-    c = 0
-    topic, message=client.get('70B3D57ED00012B2/devices/00000000AE6C63E4/up')
+    # client.host = 'staging.thethingsnetwork.org'
+    # client.port = '1883'
+    # client.username = '70B3D57ED00012B2'
+    # client.password = 'c8iuTSccnypK1eoFzEb/OoqB2FVAiFg/aEaYesnNf4w='
+    # client.connect
+    # c = 0
+    # topic, message=client.get('70B3D57ED00012B2/devices/00000000AE6C63E4/up')
+    #
+    # puts mqtttemperature = JSON.parse(message)['fields']['temperature']
+    mqtttemperature = MqttReader.read()
 
-    puts mqtttemperature = JSON.parse(message)['fields']['temperature']
     c = mqtttemperature.to_f
   end
 
---
+++ lib/mqtt_reader.rb
@@ -0,0 +1,15 @@
+require 'mqtt'
+class MqttReader
+def self.read()
+  client = MQTT::Client.new
+  client.host = 'staging.thethingsnetwork.org'
+  client.port = '1883'
+  client.username = '70B3D57ED00012B2'
+  client.password = 'c8iuTSccnypK1eoFzEb/OoqB2FVAiFg/aEaYesnNf4w='
+  client.connect
+  c = 0
+  topic, message=client.get('70B3D57ED00012B2/devices/00000000AE6C63E4/up')
+
+  return JSON.parse(message)['fields']['temperature']
+end
+end
